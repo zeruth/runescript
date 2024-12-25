2bd83570e9a33dd97bca81020f9a7d0cb3eeb255
GitHub Action <action@github.com>
2024-12-25 05:51:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/zeruth/runescript/MyBundle.kt
+++ src/main/kotlin/com/github/zeruth/runescript/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zeruth.runescript
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zeruth/runescript/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/zeruth/runescript/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.zeruth.runescript.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.zeruth.runescript.MyBundle
+import com.github.zeruth.runescript.services.MyProjectService
 import javax.swing.JButton
 
 
